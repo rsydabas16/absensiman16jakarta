ce9266dd17faa733cf2983a57a4b1caf87091c41
rsydabas16 <151610327+rsydabas16@users.noreply.github.com>
2025-06-24 13:49:53 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,10 +5,19 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Services\TelegramService;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
     // ...
+
+
+public function boot()
+{
+    if (env('APP_ENV') !== 'local') {
+        URL::forceScheme('https');
+    }
+}
 
     public function register(): void
     {
